6179cc6872a7ae22d20178f744d0993ffd0989ce
syamsul muttaqin <lombok.oc@gmail.com>
2015-08-25 18:27:11 +0700

    initial commit

---
+++ coba.php
@@ -0,0 +1,25 @@
+<?php
+include 'vendor/autoload.php';
+
+use JonnyW\PhantomJs\Client;
+use JonnyW\PhantomJs\DependencyInjection\ServiceContainer;
+
+$serviceContainer = ServiceContainer::getInstance();
+$procedureLoader = $serviceContainer->get('procedure_loader_factory')->createProcedureLoader(dirname(__FILE__));
+
+$client = Client::getInstance();
+$client->getProcedureLoader()->addLoader($procedureLoader);
+
+$req = $client->getMessageFactory()->createRequest();
+$res = $client->getMessageFactory()->createResponse();
+
+$req->setType('procedure');
+$fragment = 'customers';
+$req->setUrl('http://localhost/demoapp/#/'.$fragment);
+
+$client->send($req, $res);
+
+ob_start();
+echo $res->getContent();
+file_put_contents('snapshot/'.$fragment,ob_get_contents());
+ob_end_flush();
